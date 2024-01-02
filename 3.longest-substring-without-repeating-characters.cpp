17f06565dd3c669ec82570dad9c40f0190410c01
cxinu <cxinu3099@gmail.com>
2024-01-02 11:58:35 +0530

    Add solution for finding the length of the longest substring without repeating characters

--- 3.longest-substring-without-repeating-characters.cpp
+++ 3.longest-substring-without-repeating-characters.cpp
@@ -6,13 +6,26 @@
 
 // @lc code=start
 #include <string>
+#include <unordered_set>
 using namespace std;
 
 class Solution {
   public:
     int lengthOfLongestSubstring(string s) {
-        if (s.empty())
-            return 0;
+        unordered_set<char> win_set;
+        int l = 0, r = 0, max_len = 0;
+
+        while (r < s.size()) {
+            if (win_set.find(s[r]) == win_set.end()) {
+                win_set.insert(s[r]);
+                r++;
+                max_len = max(max_len, r - l);
+            } else {
+                win_set.erase(s[l]);
+                l++;
+            }
+        }
+        return max_len;
     }
 };
 // @lc code=end
